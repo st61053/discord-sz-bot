bda43a3a235e78f8eea99c137d3bb3f6694c3be7
Matěj Varga <varga.matej@seznam.cz>
2023-04-14 23:17:17 +0200

    fix

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ client.on('interactionCreate', async (interaction) => {
       const userRef = db.collection('users').doc(userId);
       await userRef.set({
         id: userId,
-        messageCount: admin.firestore.FieldValue.increment(points)
+        messageCount: admin.firestore.FieldValue.decrement(points)
       }, { merge: true });
 
       const user = await client.users.fetch(userId);

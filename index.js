4437ba9318e7d993d08b586452e210e06d4ce37f
Matěj Varga <varga.matej@seznam.cz>
2023-05-04 19:58:35 +0200

    fast

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ client.once(Events.ClientReady, c => {
 
 var cron = require('node-cron');
 
-cron.schedule('0 20 * * *', () => {
+cron.schedule('0 8 * * *', () => {
   if (detective) {
     detectivePike();
   }

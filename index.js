6ba10f635ae88e76d6c555a0dcd596ea9ba6723f
Matěj Varga <varga.matej@seznam.cz>
2023-05-05 12:15:44 +0200

    try

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const token = process.env.TOKEN;
 
 const detective = true;
 
-let answer = null;
-let reward = [25, 20, 15, 10, 5];
-let answerPlayers = [];
-let winnerCount = 0;
-let playerTrys = {};
+// let answer = null;
+// let reward = [25, 20, 15, 10, 5];
+// let answerPlayers = [];
+// let winnerCount = 0;
+// let playerTrys = {};
 
 // Create a new client instance
 const client = new Client({
